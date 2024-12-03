720f38d6dc66e2dfe6c1098b5f9307245bf4d5bc
Tanvi <tanvivchiman@gmail.com>
2024-12-03 15:24:53 +0530

    clean functions code with input

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,26 @@
 'use client' 
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [name,setName]=useState("")
+  const [age, setAge]=useState("")
+  const callUser=()=>{
+    setName("Tanvi")
+    setAge(22)
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Home Page
-        <User name="Tanvi"/>
-        <button onClick={HelloCall}>Click me</button>
+        <h1>User Name: {name} </h1>
+        <h1>User's Age: {age} </h1>
+
+        {/* <input type="text" onChange={(e)=> setName(e.target.value)} /> */}
+        <button onClick={()=>callUser()} >Click me</button>
       </main>
-    
     </div> 
   );
-}
-
-const HelloCall=()=>{alert("Hello World!")}
-
-const User=(props)=>{
-return(
-  <div>
-    <h2>User is {props.name} </h2>
-  </div>
-)
 }
